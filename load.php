1cc7ffc3760f6f4e2b516553f12cb452547c507f
Shan-Li Wang <59712863+kimberly332@users.noreply.github.com>
2021-04-01 01:18:15 -0400

    Update load.php

--- load.php
+++ load.php
@@ -4,10 +4,10 @@ define('ADMIN_PATH', ABSPATH. '/includes/admin');
 define('ADMIN_SCRIPT_PATH', ADMIN_PATH. '/scripts');
 
 # Turn display error on, DEBUG ONLY!! Please turn it off in your production env
-ini_set('display_errors', 1);
+#ini_set('display_errors', 1);
 
 # Create a session
-session_start();
+#session_start();
 
 require_once ABSPATH.'/includes/config/database.php';
 // require_once ADMIN_SCRIPT_PATH.'/read.php';
